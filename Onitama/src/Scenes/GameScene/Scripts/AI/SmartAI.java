8824a306f6d54a9488d2be885c4ef5e1efbc4a7d
Do Duc Anh <doducanh2810@gmail.com>
2024-05-16 13:34:25 +0200

    Change eval to integer

--- Onitama/src/Scenes/GameScene/Scripts/AI/SmartAI.java
+++ Onitama/src/Scenes/GameScene/Scripts/AI/SmartAI.java
@@ -13,14 +13,14 @@ import Onitama.src.Scenes.GameScene.Scripts.Turn;
  * SmartAI
  */
 public class SmartAI extends AI {
-    private static final double minusINF = Double.NEGATIVE_INFINITY;
-    private static final double plusINF = Double.POSITIVE_INFINITY;
+    private static final int minusINF = Integer.MIN_VALUE;
+    private static final int plusINF = Integer.MAX_VALUE;
     private static final Vector2D center = new Vector2D(2, 2);
-    private static final double[] defaultWeights = {4, 3, 2, 1};
+    private static final int[] defaultWeights = {4, 3, 2, 1};
     public static final int NB_METHOD = 4;
 
     @SuppressWarnings("unchecked")
-    private static final Function<GameConfiguration, Double>[] evaluations = new Function[NB_METHOD];
+    private static final Function<GameConfiguration, Integer>[] evaluations = new Function[NB_METHOD];
     static {
         evaluations[0] = config -> pieceNumber(config);
         evaluations[1] = config -> kingSafety(config);
@@ -30,14 +30,14 @@ public class SmartAI extends AI {
 
     Turn bestMove;
     int difficulty, selfID;
-    double[] weights;
+    int[] weights;
 
 
     public SmartAI(int difficulty, int player) {
         this(difficulty, player, defaultWeights);
     }
 
-    public SmartAI(int difficulty, int player, double[] weights) {
+    public SmartAI(int difficulty, int player, int[] weights) {
         this.difficulty = difficulty;
         this.selfID = player;
         this.weights = weights;
@@ -46,18 +46,18 @@ public class SmartAI extends AI {
 
     @Override
     public Turn play() {
-        double eval = minmax(GameScene.game, true, difficulty, 
+        int eval = minmax(GameScene.game, true, difficulty, 
                              minusINF, plusINF);
         System.err.println("Best score found: " + eval);
         return bestMove;
     }
 
-    private double minmax(GameConfiguration config, boolean isMaximizing, 
-                                    int depth, double alpha, double beta) {
+    private int minmax(GameConfiguration config, boolean isMaximizing, 
+                                    int depth, int alpha, int beta) {
         if (config.isGameOver() || depth == 0) 
             return heuristic(config, depth);
 
-        double eval, maxEval, minEval;
+        int eval, maxEval, minEval;
 
         if (isMaximizing) {
             maxEval = minusINF;
@@ -89,8 +89,8 @@ public class SmartAI extends AI {
     }
 
 
-    private double heuristic(GameConfiguration config, int depth) {
-        double eval = 0; // evaluation for AI
+    private int heuristic(GameConfiguration config, int depth) {
+        int eval = 0; // evaluation for AI
 
         if (config.isGameOver()) // AI lost
             eval -= (1000000 + depth);
@@ -107,11 +107,11 @@ public class SmartAI extends AI {
 
     /* Methods to evaluate a state of the game  */
 
-    private static double pieceNumber(GameConfiguration config) {
+    private static int pieceNumber(GameConfiguration config) {
         return config.allyPositions().size() - config.enemyPositions().size();
     }
 
-    private static double kingSafety(GameConfiguration config) {
+    private static int kingSafety(GameConfiguration config) {
         int allyDanger = 0;
         for (Vector2D enemy : config.enemyPositions())
             allyDanger += distance(config.allyKing(), enemy);
@@ -121,12 +121,12 @@ public class SmartAI extends AI {
         return enemyDanger - allyDanger;        
     }
 
-    private static double throneDistance(GameConfiguration config) {
+    private static int throneDistance(GameConfiguration config) {
         return distance(config.allyKing(), config.allyGoal()) - 
                distance(config.enemyGoal(), config.enemyKing());
     }
 
-    private static double centerDistance(GameConfiguration config) {
+    private static int centerDistance(GameConfiguration config) {
         int allyDist = 0;
         for (Vector2D ally : config.allyPositions())
             allyDist += distance(ally, center);
--- Onitama/src/Scenes/GameScene/Scripts/AI/Training.java
+++ Onitama/src/Scenes/GameScene/Scripts/AI/Training.java
@@ -1,9 +1,27 @@
 package Onitama.src.Scenes.GameScene.Scripts.AI;
 
+import java.util.Random;
+
 /**
  * Training
  */
 public class Training {
+    Random random = new Random();
+    
+    SmartAI generateAI() {
+        return null;
+    }
+
+    SmartAI mate(SmartAI father, SmartAI mother) {
+        int[] weights = new int[SmartAI.NB_METHOD];
+        for (int i = 0; i < SmartAI.NB_METHOD; i++)
+            weights[i] = (father.weights[i] + mother.weights[i]) / 2;
+        return new SmartAI(father.difficulty, father.selfID, weights);
+    }
+
+    SmartAI mutate(SmartAI original) {
+        return null;
+    }
 
     public static void main(String[] args) {
         
