ea854a9b6ebe1c9492e30d2f8c06772cebf9c314
Do Duc Anh <doducanh2810@gmail.com>
2024-05-18 08:24:24 +0200

    Progress on training

--- Onitama/src/Scenes/GameScene/Scripts/AI/Training.java
+++ Onitama/src/Scenes/GameScene/Scripts/AI/Training.java
@@ -6,24 +6,40 @@ import java.util.Random;
  * Training
  */
 public class Training {
-    Random random = new Random();
+    static Random random = new Random();
+    static int difficulty = 6;
+    static int populationSize = 100;
     
-    SmartAI generateAI() {
-        return null;
+    static SmartAI generateAI() {
+        int[] weights = new int[SmartAI.NB_METHOD];
+        for (int i = 0; i < SmartAI.NB_METHOD; i++)
+            weights[i] = random.nextInt();
+        return new SmartAI(difficulty, 0, weights);
     }
 
-    SmartAI mate(SmartAI father, SmartAI mother) {
+    static SmartAI mate(SmartAI father, SmartAI mother) {
         int[] weights = new int[SmartAI.NB_METHOD];
         for (int i = 0; i < SmartAI.NB_METHOD; i++)
             weights[i] = (father.weights[i] + mother.weights[i]) / 2;
-        return new SmartAI(father.difficulty, father.selfID, weights);
+        return new SmartAI(difficulty, 0, weights);
     }
 
-    SmartAI mutate(SmartAI original) {
-        return null;
+    static SmartAI mutate(SmartAI original) {
+        int index = random.nextInt(SmartAI.NB_METHOD);
+        int variation = random.nextInt(original.weights[index]);
+        if (random.nextBoolean())
+            original.weights[index] += variation;
+        else
+            original.weights[index] -= variation; 
+        return original;
     }
 
     public static void main(String[] args) {
-        
+        // Intialize population
+        SmartAI[] population = new SmartAI[populationSize];
+        for (int i = 0; i < populationSize; i++)
+            population[i] = generateAI();
+
+        // TODO
     }
 }
