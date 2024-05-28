33b13b302da997c2deed551a6110b4c1bce31259
Mascarenhas Dalle Nery Rafael <mascarer@im2ag-204-04.u-ga.fr>
2024-05-28 13:18:06 +0200

    wip in game menu

--- Onitama/src/Scenes/GameScene/Interface/InGameMenu.java
+++ Onitama/src/Scenes/GameScene/Interface/InGameMenu.java
@@ -1,9 +1,17 @@
 package Onitama.src.Scenes.GameScene.Interface;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.KeyEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
 import Engine.Entities.UI.BlurredArea;
+import Engine.Entities.UI.FlatButton;
 import Engine.Entities.UI.MenuFrame;
 import Engine.Structures.Vector2D;
 import Onitama.src.Main;
@@ -25,6 +33,30 @@ public class InGameMenu extends MenuFrame {
 
         Main.gameScene.setComponentZOrder(blurredArea, 0);
 
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        add(Box.createVerticalGlue());
+
+        FlatButton resumeButton = createBaseButton(" Resume ");
+        resumeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(resumeButton);
+
+        add(Box.createVerticalStrut(6));
+
+        
+        FlatButton restartButton = createBaseButton("Restart ");
+        restartButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(restartButton);
+
+        add(Box.createVerticalStrut(6));
+        
+        FlatButton quitButton = createBaseButton("  Quit  ");
+        quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(quitButton);
+
+        
+
+        add(Box.createVerticalGlue());
     }
 
     @Override
@@ -34,6 +66,19 @@ public class InGameMenu extends MenuFrame {
             Main.gameScene.removeComponent(this);
             Main.gameScene.setEnabledGUI(true);
         }
+    }
+
+    private FlatButton createBaseButton(String content) {
+        FlatButton button = new FlatButton(content);
+        button.setFont(Main.FontManager.getDefaultCustomFont(Font.BOLD, 16));
+        button.setForeground(Main.Palette.foreground);
+        button.setMainColor(Main.Palette.selection.brighter());
+        button.setAccentColor(new Color(255,255,255,25));
+        button.setCurvature(20, 20);
+        button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        button.setSize(20, 20);
+        button.setFocusable(false);
+        return button;
     }
     
 }
